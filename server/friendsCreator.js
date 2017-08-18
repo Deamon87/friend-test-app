8980d4d381c41fa47122dd6a3fa9aefd7dd27f12
Dmitry Tolmachov <d.tolmachov@aimprosoft.com>
2017-08-18 12:39:06 +0300

    cookie session and friendList creator

---
+++ server/friendsCreator.js
@@ -0,0 +1,12 @@
+module.exports = function () {
+    var friendsList = [];
+    for (var i = 0; i < 50; i++) {
+        friendsList.push({
+            name : 'Friend ' + i,
+            age: Math.random()*40+15,
+            gender: Math.random() > 0.5 ? 'male' : 'female'
+        })
+    }
+
+    return friendsList;
+};
--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var path = require('path');
+var session = require('express-session');
+var friendListCreator = require(friendListCreator);
 var app = express();
+
+var friendsList = friendListCreator();
 
 //Serve static files
 app.use('/static', express.static(path.join(__dirname, 'static')));
@@ -15,10 +19,37 @@ app.use(function(req, res, next) {
 });
 
 //Router for API
+app.use(session({
+    secret: 'secret-some',
+    cookie: {maxAge: 60000},
+    resave: false,
+    saveUninitialized: true
+}))
+
+function checkUser(req, res, next) {
+    if (!req.session.isLogged) {
+        res.status(403).send('Not authorized');
+    } else {
+        next();
+    }
+}
+
 var router = express.Router();
 router.post("/login", function(req, res) {
     //login endpoint called
+    req.session.isLogged = true;
     res.send({login: "ok"});
+});
+
+router.get("/logout", function(req, res) {
+    //login endpoint called
+    req.session.isLogged = false;
+    res.send({login: "loggedout"});
+});
+
+
+router.get("/friends", checkUser, function (req, res) {
+
 });
 
 app.use('/api', router);
